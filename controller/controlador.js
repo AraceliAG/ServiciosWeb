120a097f9b0bb59340e812c885c3e7319aca1138
AraceliAG <131417012+AraceliAG@users.noreply.github.com>
2024-03-16 01:49:41 -0600

    .
    .

--- controller/controlador.js
+++ controller/controlador.js
@@ -1,48 +1,56 @@
-const cargarDatos = require('./model');
+const cargarDatos = require('../model/modelo')
 const plotly = require('plotly')('username', 'apiKey');
 const fs = require('fs');
 
-function generarGrafico(desdeArchivo = true) {
-    // Cargar los datos
-    const datos = cargarDatos(desdeArchivo);
 
-    // Separar los datos de x e y
-    const x = datos.map(fila => fila[7]);
-    const y = datos.map(fila => parseFloat(fila[2]));
+module.exports = {
+    generarGrafico:function (desdeArchivo = true) {
+        // Cargar los datos
+        const datos = cargarDatos(desdeArchivo);
+    
+        // Separar los datos de x e y
+        const x = datos.map(fila => fila[7]);
+        const y = datos.map(fila => parseFloat(fila[2]));
+    
+        // Crear la figura
+        const trace = {
+            x: x,
+            y: y,
+            mode: 'markers',
+            type: 'scatter'
+        };
+    
+        // Diseñar el diseño del gráfico
+        const layout = {
+            title: 'Gráfico interactivo de temperatura real',
+            xaxis: { title: 'Timestamp' },
+            yaxis: { title: 'Temperatura Real (°C)' },
+            hovermode: 'closest'
+        };
+    
+        const graphOptions = {
+            layout: layout,
+            filename: 'grafico_interactivo',
+            fileopt: 'overwrite'
+        };
+    
+        // Guardar el gráfico en un archivo JSON
+        plotly.plot([trace], graphOptions, function (err, msg) {
+            if (err) return console.log(err);
+            console.log(msg);
+            fs.writeFileSync('grafico_interactivo.json', JSON.stringify(msg));
+        });
+    },
+    
+     mostrarGrafico:function(res, req) {
+        // No es necesario abrir el archivo en el navegador cuando se genera en Node.js
+        // console.log("El archivo del gráfico interactivo se ha generado correctamente.");
+        res.render('index', { title: 'Express' });
+    }
 
-    // Crear la figura
-    const trace = {
-        x: x,
-        y: y,
-        mode: 'markers',
-        type: 'scatter'
-    };
-
-    // Diseñar el diseño del gráfico
-    const layout = {
-        title: 'Gráfico interactivo de temperatura real',
-        xaxis: { title: 'Timestamp' },
-        yaxis: { title: 'Temperatura Real (°C)' },
-        hovermode: 'closest'
-    };
-
-    const graphOptions = {
-        layout: layout,
-        filename: 'grafico_interactivo',
-        fileopt: 'overwrite'
-    };
-
-    // Guardar el gráfico en un archivo JSON
-    plotly.plot([trace], graphOptions, function (err, msg) {
-        if (err) return console.log(err);
-        console.log(msg);
-        fs.writeFileSync('grafico_interactivo.json', JSON.stringify(msg));
-    });
 }
 
-function mostrarGrafico() {
-    // No es necesario abrir el archivo en el navegador cuando se genera en Node.js
-    console.log("El archivo del gráfico interactivo se ha generado correctamente.");
-}
 
-module.exports = { generarGrafico, mostrarGrafico };
+
+
+// module.exports = { generarGrafico, mostrarGrafico };
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const controlador = require("../controller/controlador");
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+
+router.get('/',controlador.mostrarGrafico);
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
 
 module.exports = router;
