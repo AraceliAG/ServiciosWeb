bc67fda8337ef5f5a6261c829b4396a0bd4c3234
AraceliAG <131417012+AraceliAG@users.noreply.github.com>
2024-04-13 19:24:40 -0600

    ya quedo

--- public/stylesheets/javascripts/grafica4.js
+++ public/stylesheets/javascripts/grafica4.js
@@ -1,14 +1,3 @@
-// Datos de ejemplo (reemplaza esto con tus datos)
-var chartData = [
-    {"time": "2018-01-01", "value": 11}, {"time": "2018-01-02", "value": 10}, 
-    {"time": "2018-01-03", "value": 10}, {"time": "2018-01-04", "value": 9}, 
-    {"time": "2018-01-05", "value": 11}, {"time": "2018-01-06", "value": 8}, 
-    {"time": "2018-01-07", "value": 8}, {"time": "2018-01-08", "value": 7}, 
-    {"time": "2018-01-09", "value": 6}, {"time": "2018-01-10", "value": 8},
-    {"time": "2018-01-11", "value": 12}, {"time": "2018-01-12", "value": 4},
-    {"time": "2018-01-13", "value": 3}, {"time": "2018-01-14", "value": 6},
-    {"time": "2018-01-15", "value": 3}, {"time": "2018-01-16", "value": 4},
-];
 
 // Configurar la gráfica
 var ctx = document.getElementById('myChart').getContext('2d');
@@ -28,3 +17,4 @@ var myChart = new Chart(ctx, {
         // Opciones de configuración adicionales (puedes personalizar según tus necesidades)
     }
 });
+
--- public/stylesheets/javascripts/test.py
+++ public/stylesheets/javascripts/test.py
@@ -1,30 +1,43 @@
-import pandas as pd
 import json
 import datetime
+import os
 
+import pandas as pd
+
+# Leer los datos del archivo CSV
 data = pd.read_csv("datos_tonala_2.csv")
 
-#CONVERSION A INT
-# Temperatura a números y quitar el °
+# Convertir la columna 'temperatura' a números y quitar el símbolo de grado
 data['temperatura'] = data['temperatura'].str.replace('°', '').astype(int)
 
-# FUNCION 
+# Función para parsear las fechas
 def custom_date_parser(date_string):
-    # Dividir la cadena en: día de la semana, día, mes, año
     parts = date_string.split(' ')
-    # Mapear nombres de los meses en español a números de mes
     months_es = {'Enero': 1, 'Febrero': 2, 'Marzo': 3, 'Abril': 4, 'Mayo': 5, 'Junio': 6, 'Julio': 7, 'Agosto': 8, 'Septiembre': 9, 'Octubre': 10, 'Noviembre': 11, 'Diciembre': 12}
-    # Crear un objeto datetime
     date = datetime.datetime(int(parts[3]), months_es[parts[2]], int(parts[1]))
     return date
 
+# Aplicar la función de parsing a la columna 'fecha'
 data['fecha'] = data['fecha'].apply(custom_date_parser)
 
 # Generar datos de la gráfica en el formato requerido por LightweightCharts
-chart_data = [{'time': date.strftime('%Y-%m-%d'), 'value': temp} for date, temp in zip(data['fecha'], data['temperatura'])]
+charData = [{'time': date.strftime('%Y-%m-%d'), 'value': temp} for date, temp in zip(data['fecha'], data['temperatura'])]
 
-# Escribir los datos en un archivo JavaScript (modo append)
-with open('grafica3.js', 'a') as f:
-    f.write("\nvar chart_data = ")
-    json.dump(chart_data, f)
-    f.write(";\n")
+# Verificar si el archivo JavaScript existe y contiene datos
+if os.path.exists('grafica4.js') and os.path.getsize('grafica4.js') > 0:
+    # LEER EL CONTENIDO ACTUAL DEL ARCHIVO JAVASCRIPT
+    with open('grafica4.js', 'r') as f:
+        existing_code = f.read()
+
+    # ABRIR EL ARCHIVO JAVASCRIPT EN MODO ESCRITURA Y SOBRESCRIBIR EL NUEVO CODIGO
+    with open('grafica4.js', 'w') as f:
+        f.write("var charData = ")
+        json.dump(charData, f)
+        f.write(";\n")
+        f.write(existing_code)
+else:
+    # Escribir los datos en un nuevo archivo JavaScript
+    with open('grafica4.js', 'w') as f:
+        f.write("var chart_data = ")
+        json.dump(charData, f)
+        f.write(";\n")
