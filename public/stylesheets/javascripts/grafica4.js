6cd6d19ccfbf57f8bc460187677e4ca9fe4ea88c
AraceliAG <131417012+AraceliAG@users.noreply.github.com>
2024-04-13 20:55:51 -0600

    Visualizacion Grafica

--- public/stylesheets/javascripts/grafica4.js
+++ public/stylesheets/javascripts/grafica4.js
@@ -3,16 +3,21 @@ var ctx = document.getElementById('myChart').getContext('2d');
 var myChart = new Chart(ctx, {
     type: 'line',
     data: {
-        labels: chartData.map(data => data.time), // Suponiendo que 'time' es la etiqueta de tu eje X
+        labels: chartData.map(data => data.time),
         datasets: [{
-            label: 'Valor',
-            data: chartData.map(data => data.value), // Suponiendo que 'value' es tu dato en el eje Y
-            backgroundColor: 'rgba(255, 99, 132, 0.2)', // Color de fondo del gráfico
-            borderColor: 'rgba(255, 99, 132, 1)', // Color de la línea del gráfico
-            borderWidth: 1 // Ancho de la línea del gráfico
+            label: 'TEMPERATURA',
+            data: chartData.map(data => data.value),
+            backgroundColor: 'rgba(255, 99, 132, 0.2)',
+            borderColor: 'rgba(255, 99, 132, 1)',
+            borderWidth: 1
         }]
     },
     options: {
-        // Opciones de configuración adicionales (puedes personalizar según tus necesidades)
+        responsive: true,
+        maintainAspectRatio: false,
+        width: '100%', // Ocupar todo el ancho del contenedor
+        height: '100%' // Ocupar todo el alto del contenedor
     }
 });
+
+
