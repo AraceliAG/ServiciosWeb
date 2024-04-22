c428b148649497b6a8b349fdbeabbb5c7e80845c
AraceliAG <131417012+AraceliAG@users.noreply.github.com>
2024-04-22 00:03:12 -0600

    .
    .

--- controller/controller1.js
+++ controller/controller1.js
@@ -22,5 +22,22 @@ module.exports={
         // Enviar datos mediante redirección con parámetros de consulta
         res.redirect(`/grafica?anio=${anio}&mes=${mes}`);
 
+    },
+
+    graficaP:function (req, res){
+
+        res.render('graficaP');
+    },
+    graficaDP: function(req, res) {
+  
+        const anio = req.body.anioSeleccionado
+        const mes = req.body.mesSeleccionado
+        console.log('anio', anio)
+        console.log('mes', mes)
+        
+        //res.redirect('grafica');
+        // Enviar datos mediante redirección con parámetros de consulta
+        res.redirect(`/graficaP?anio=${anio}&mes=${mes}`);
+
     }
 }
--- routes/index.js
+++ routes/index.js
@@ -7,5 +7,7 @@ const controller = require ("../controller/controller1")
 router.get('/', controller.index)
 router.get('/grafica', controller.grafica)
 router.post('/grafica', controller.graficaD)
+router.get('/graficaP', controller.graficaP)
+router.post('/graficaP', controller.graficaDP)
 
 module.exports = router;
