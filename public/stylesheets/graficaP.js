8d83b514ee440d13ef813d8a832bfd77d3b2ec59
AraceliAG <131417012+AraceliAG@users.noreply.github.com>
2024-04-27 13:13:02 -0600

    .
    .

--- public/stylesheets/graficaP.js
+++ public/stylesheets/graficaP.js
@@ -8,8 +8,8 @@ var chartData32 = [{"time": "2018-01-01", "value": 1026}, {"time": "2018-01-02",
 
 // OBTENCION DE DATOS DESDE EL CONTROLADOR 
 const urlParams = new URLSearchParams(window.location.search);
-const anio2 = urlParams.get('anio');
-const mes2 = urlParams.get('mes');
+const anio2 = urlParams.get('anio2');
+const mes2 = urlParams.get('mes2');
 const seccion2 = urlParams.get('seccion');
 
 // VER EN CONSOLA LOS DATOS 
