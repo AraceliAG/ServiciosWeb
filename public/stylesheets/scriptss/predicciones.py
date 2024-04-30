3143f3229cab5c083ff21a21c1adaef403961b0d
TACODEBISTECK <167110182+DIABLOTE69@users.noreply.github.com>
2024-04-29 22:04:31 -0600

    algoritmo predicciones
    continuacion

--- public/stylesheets/scriptss/predicciones.py
+++ public/stylesheets/scriptss/predicciones.py
@@ -1,6 +1,10 @@
 from datetime import datetime
 import matplotlib.pyplot as plt
 from meteostat import Point, Daily
+import openmeteo_requests
+import requests_cache
+import pandas as pd
+from retry_requests import retry
 
 # Establecer periodo de tiempo
 inicio = datetime(2015, 1, 12)
@@ -13,4 +17,113 @@ tonala = Point(20.6667, -103.3833, 1570)
 data = Daily(tonala, inicio, fin)
 data = data.fetch()
 #print(data) #TODO BIEN ACA SOLO IMPRIME LOS PARAMETROS JUNTO A LA FECHA
-print(data.head())
+# print(data.head()) #TODO BIEN LAS CABECERAS
+data.to_csv('2018-2023.csv', index=True) # Para guardar sin el índice del DataFrame
+# Setup the Open-Meteo API client with cache and retry on error
+cache_session = requests_cache.CachedSession('.cache', expire_after = -1)
+retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
+openmeteo = openmeteo_requests.Client(session = retry_session)
+# Make sure all required weather variables are listed here
+# The order of variables in hourly or daily is important to assign them correctly below
+url = "https://archive-api.open-meteo.com/v1/archive"
+params = {
+	"latitude": 20.6244,
+	"longitude": -103.2342,
+	"start_date": "2000-01-01",
+	"end_date": "2009-12-31",
+	"daily": ["temperature_2m_max", "temperature_2m_min", "temperature_2m_mean", "precipitation_sum", "rain_sum", "snowfall_sum", "precipitation_hours", "wind_speed_10m_max"]
+}
+responses = openmeteo.weather_api(url, params=params)
+
+# Process first location. Add a for-loop for multiple locations or weather models
+response = responses[0]
+# print(f"Coordinates {response.Latitude()}°N {response.Longitude()}°E")
+# print(f"Elevation {response.Elevation()} m asl")
+# print(f"Timezone {response.Timezone()} {response.TimezoneAbbreviation()}")
+# print(f"Timezone difference to GMT+0 {response.UtcOffsetSeconds()} s")
+
+# Process daily data. The order of variables needs to be the same as requested.
+daily = response.Daily()
+daily_temperature_2m_max = daily.Variables(0).ValuesAsNumpy()
+daily_temperature_2m_min = daily.Variables(1).ValuesAsNumpy()
+daily_temperature_2m_mean = daily.Variables(2).ValuesAsNumpy()
+daily_precipitation_sum = daily.Variables(3).ValuesAsNumpy()
+daily_rain_sum = daily.Variables(4).ValuesAsNumpy()
+daily_snowfall_sum = daily.Variables(5).ValuesAsNumpy()
+daily_precipitation_hours = daily.Variables(6).ValuesAsNumpy()
+daily_wind_speed_10m_max = daily.Variables(7).ValuesAsNumpy()
+
+daily_data = {"date": pd.date_range(
+	start = pd.to_datetime(daily.Time(), unit = "s", utc = True),
+	end = pd.to_datetime(daily.TimeEnd(), unit = "s", utc = True),
+	freq = pd.Timedelta(seconds = daily.Interval()),
+	inclusive = "left"
+)}
+daily_data["temperature_2m_max"] = daily_temperature_2m_max
+daily_data["temperature_2m_min"] = daily_temperature_2m_min
+daily_data["temperature_2m_mean"] = daily_temperature_2m_mean
+daily_data["precipitation_sum"] = daily_precipitation_sum
+daily_data["rain_sum"] = daily_rain_sum
+daily_data["snowfall_sum"] = daily_snowfall_sum
+daily_data["precipitation_hours"] = daily_precipitation_hours
+daily_data["wind_speed_10m_max"] = daily_wind_speed_10m_max
+
+daily_dataframe_10 = pd.DataFrame(data = daily_data)
+# print(daily_dataframe_10)
+daily_dataframe_10.to_csv('2000 - 2010 - APIH2.csv', index=True) # Para guardar sin el índice del DataFrame
+# Setup the Open-Meteo API client with cache and retry on error
+cache_session = requests_cache.CachedSession('.cache', expire_after = -1)
+retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
+openmeteo = openmeteo_requests.Client(session = retry_session)
+
+# Make sure all required weather variables are listed here
+# The order of variables in hourly or daily is important to assign them correctly below
+url = "https://archive-api.open-meteo.com/v1/archive"
+params = {
+	"latitude": 20.6244,
+	"longitude": -103.2342,
+	"start_date": "2010-01-01",
+	"end_date": "2019-12-31",
+	"daily": ["temperature_2m_max", "temperature_2m_min", "temperature_2m_mean", "precipitation_sum", "rain_sum", "snowfall_sum", "precipitation_hours", "wind_speed_10m_max"]
+}
+responses = openmeteo.weather_api(url, params=params)
+
+# Process first location. Add a for-loop for multiple locations or weather models
+response = responses[0]
+# print(f"Coordinates {response.Latitude()}°N {response.Longitude()}°E")
+# print(f"Elevation {response.Elevation()} m asl")
+# print(f"Timezone {response.Timezone()} {response.TimezoneAbbreviation()}")
+# print(f"Timezone difference to GMT+0 {response.UtcOffsetSeconds()} s")
+
+# Process daily data. The order of variables needs to be the same as requested.
+daily = response.Daily()
+daily_temperature_2m_max = daily.Variables(0).ValuesAsNumpy()
+daily_temperature_2m_min = daily.Variables(1).ValuesAsNumpy()
+daily_temperature_2m_mean = daily.Variables(2).ValuesAsNumpy()
+daily_precipitation_sum = daily.Variables(3).ValuesAsNumpy()
+daily_rain_sum = daily.Variables(4).ValuesAsNumpy()
+daily_snowfall_sum = daily.Variables(5).ValuesAsNumpy()
+daily_precipitation_hours = daily.Variables(6).ValuesAsNumpy()
+daily_wind_speed_10m_max = daily.Variables(7).ValuesAsNumpy()
+
+daily_data = {"date": pd.date_range(
+	start = pd.to_datetime(daily.Time(), unit = "s", utc = True),
+	end = pd.to_datetime(daily.TimeEnd(), unit = "s", utc = True),
+	freq = pd.Timedelta(seconds = daily.Interval()),
+	inclusive = "left"
+)}
+daily_data["temperature_2m_max"] = daily_temperature_2m_max
+daily_data["temperature_2m_min"] = daily_temperature_2m_min
+daily_data["temperature_2m_mean"] = daily_temperature_2m_mean
+daily_data["precipitation_sum"] = daily_precipitation_sum
+daily_data["rain_sum"] = daily_rain_sum
+daily_data["snowfall_sum"] = daily_snowfall_sum
+daily_data["precipitation_hours"] = daily_precipitation_hours
+daily_data["wind_speed_10m_max"] = daily_wind_speed_10m_max
+
+daily_dataframe = pd.DataFrame(data = daily_data)
+# print(daily_dataframe)
+daily_dataframe.to_csv('2010 - 2022 - APIH.csv', index=True) # Para guardar sin el índice del DataFrame
+df_2000_2010 = pd.read_csv('2000 - 2010 - APIH.csv')
+df_2010_2022 = pd.read_csv('2010 - 2022 - APIH.csv')
+df_2000_2010.head()
