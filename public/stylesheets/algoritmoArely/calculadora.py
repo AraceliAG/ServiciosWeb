8e016d9c9af5480901bdbc59f517f0eb2dcb4baf
AraceliAG <131417012+AraceliAG@users.noreply.github.com>
2024-05-07 12:50:11 -0600

    .
    .

--- public/stylesheets/algoritmoArely/calculadora.py
+++ public/stylesheets/algoritmoArely/calculadora.py
@@ -1,9 +1,8 @@
 import pandas as pd
 from sklearn.linear_model import Ridge
 
-
 def calculadora():
-    #daily_dataframe.to_csv('2010 - 2022 - APIH.csv', index=True) # Para guardar sin el índice del DataFrame
+   
     df_2000_2010 = pd.read_csv('2000 - 2010 - APIH.csv')
     df_2010_2022 = pd.read_csv('2010 - 2022 - APIH.csv')
     df_2000_2010.head()
@@ -14,26 +13,17 @@ def calculadora():
     df_historica = df_historica.drop(columns=['Unnamed: 0'])# Eliminar columnas que no sirven
     # Cambiar nombre de columnas
     new_columns = ['fecha', 'temp_max', 'temp_min', 'temp_prom', 'precp', 'lluvia', 'nieve', 'precp_h', 'viento']
-
     # Asignar los nuevos nombres de columnas
     df_historica.columns = new_columns
     df_historica['fecha'] = pd.to_datetime(df_historica['fecha'], errors='coerce')
-
     df_historica['fecha'] = df_historica['fecha'].dt.date
-  
     df_historica['fecha'] = pd.to_datetime(df_historica['fecha'])
- 
     nulos_totales = df_historica.isnull().sum()
-   
     registros_por_año = df_historica.groupby(df_historica['fecha'].dt.year).size()
- 
     df_historica = df_historica.set_index('fecha')
-
-    
     df_historica["target"] = df_historica.shift(-1)["temp_max"]
     df_historica = df_historica.ffill() # Solucionar el target de la ultima fecha
 
-    
     rr = Ridge(alpha=.1)
     predictors = df_historica.columns[~df_historica.columns.isin(["target"])]
     #print(predictors)
@@ -43,27 +33,17 @@ def calculadora():
         for i in range(start, df_historica.shape[0], step):
             train = df_historica.iloc[:i,:]
             test = df_historica.iloc[i:(i+step),:]
-
             model.fit(train[predictors], train["target"])
-
             preds = model.predict(test[predictors])
-
             preds = pd.Series(preds, index=test.index)
             combined = pd.concat([test["target"], preds], axis=1)
-
             combined.columns = ["actual", "prediction"]
             combined["diff"] = (combined["prediction"] - combined["actual"]).abs()
-
             all_predictions.append(combined)
-
         return pd.concat(all_predictions)
     predictions = backtest(df_historica, rr, predictors)
-    print("RESULTADO: ")
-
     valores_caracteristicas = {'temp_max': 25.613500, 'temp_min': 16.613500, 'temp_prom': 21.290585, 'precp': 2.90000, 'lluvia':2.90000, 'nieve':0.0, 'precp_h':4.0, 'viento': 9.6598}
-
     datos_prueba = pd.DataFrame([valores_caracteristicas])
-
     prediccion_2 = rr.predict(datos_prueba)
 
     def obtener_valores_caracteristicas():
@@ -85,7 +65,6 @@ def calculadora():
         return prediccion_5
 
     valores = obtener_valores_caracteristicas()
-
     prediccion_5 = hacer_prediccion( rr, valores)
     print("La predicción es:", prediccion_5)
 
